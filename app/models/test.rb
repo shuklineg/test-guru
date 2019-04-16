37b1b3802c0e55911b104c7c981decc8da153e3a
Evgenii Shuklin <shuklineg@gmail.com>
2019-04-16 22:45:21 +0500

    User instance method & Test class method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.category(title)
+    Test.select('categories.title')
+        .joins('JOIN categories ON categories.id = tests.category_id')
+        .where('categories.title = ?', title)
+        .order(:desc)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def tests(level)
+    Test.joins('JOIN questions ON questions.test_id = tests.id')
+        .joins('JOIN answers ON answers.question_id = questions.id')
+        .where(
+          'answers.user_id = :id AND tests.level = :level',
+          id: id,
+          level: level
+        )
+  end
 end
