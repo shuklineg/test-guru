fdd351742b6feca64a6e2038a06e4d75fe3238e6
Evgenii Shuklin <shuklineg@gmail.com>
2019-04-17 07:47:43 +0500

    Fix Test.category

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,9 +2,9 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.category(title)
-    Test.select('categories.title')
-        .joins('JOIN categories ON categories.id = tests.category_id')
-        .where('categories.title = ?', title)
-        .order(:desc)
+    select('title')
+      .joins('JOIN categories ON categories.id = tests.category_id')
+      .where('categories.title = ?', title)
+      .order(:desc)
   end
 end
