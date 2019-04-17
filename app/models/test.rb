e2b6d53bcc8f6adda101cd20995214bd75fcca2a
Evgenii Shuklin <shuklineg@gmail.com>
2019-04-17 09:47:34 +0500

    db/seeds

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,11 @@
 class Test < ApplicationRecord
   belongs_to :category
 
-  def self.category(title)
+  def self.titles_by_category(title)
     select('title')
       .joins('JOIN categories ON categories.id = tests.category_id')
       .where('categories.title = ?', title)
       .order(:desc)
+      .map(&:title)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  { email: 'pooh@mail.ru', encrypted_password: Digest::SHA1.hexdigest('12345') },
+  { email: 'piglet@yandex.ru', encrypted_password: Digest::SHA1.hexdigest('50 cents') },
+  { email: 'tigger@gmail.com', encrypted_password: Digest::SHA1.hexdigest('1q2w3e4r') },
+  { email: 'rabbit@gmail.com', encrypted_password: Digest::SHA1.hexdigest('carrot') },
+  { email: 'eeyore@mail.com', encrypted_password: Digest::SHA1.hexdigest('111') }
+])
+
+categories = Category.create([
+  { title: 'Объектно ориентированное программирование' },
+  { title: 'SQL' },
+  { title: 'Ruby' },
+  { title: 'Ruby On Rails' }
+])
+
+tests = Test.create([
+  { category: categories[0], title: 'Принципы ООП' },
+  { category: categories[0], title: 'Объект и класс' },
+  { category: categories[1], title: 'Оператор SELECT' },
+  { category: categories[2], title: 'Утиная типизация' },
+  { category: categories[2], title: 'Блоки' },
+  { category: categories[3], title: 'Миграции' },
+  { category: categories[3], title: 'ORM' }
+])
+
+questions = Question.create([
+  { test: tests[0], body: 'Класс является типом данных?' },
+  { test: tests[1], body: 'Объект является экземпляром класса?' },
+  { test: tests[2], body: 'Что вернет SELECT "Hello, world!"?' },
+  { test: tests[3], body: 'Если оно выглядит как утка, плавает как утка и крякает как утка, то это ...' },
+  { test: tests[4], body: 'В каких block_given? возвращает true' },
+  { test: tests[5], body: 'Зачем нужны миграции?' },
+  { test: tests[6], body: 'Что означает аббривиатура ORM?' }
+])
+
+Answer.create([
+  { question: questions[0], body: 'Да', correct: true, user: users[0] },
+  { question: questions[1], body: 'Нет', correct: false, user: users[1] },
+  { question: questions[2], body: 'Вернет выборку из одной строки содержащую "Hello, world!"', correct: true, user: users[2] },
+  { question: questions[3], body: 'Редкий вид крякающей чайки', correct: false, user: users[3] },
+  { question: questions[4], body: 'Если функции передан блок', correct: true, user: users[4] },
+  { question: questions[5], body: 'Для переселения людей', correct: false, user: users[0] },
+  { question: questions[6], body: 'Объектно-реляционное отображение', correct: true, user: users[1] }
+])
