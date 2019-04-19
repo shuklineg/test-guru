35f36a703df1b72eb024280d315be745c3812f73
Evgenii Shuklin <shuklineg@gmail.com>
2019-04-19 21:43:03 +0500

    Fix answers validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,17 @@
 class Answer < ApplicationRecord
+  MAXIMUM_ANSWES = 4
+
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
 
   validates :body, :question, presence: true
+  validate :validate_count
+
+  private
+
+  def validate_count
+    maximum = question && question.answers.size >= MAXIMUM_ANSWES
+    errors.add(:answers_count, I18n.t('answers.too_much', count: MAXIMUM_ANSWES)) if maximum
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :test, presence: true
-  validates_size_of :answers, minimum: 1, maximum: 4
 end
