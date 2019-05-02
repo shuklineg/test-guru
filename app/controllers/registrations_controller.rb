f37a1c7fa9cf29e5e571d8456c0d1bf16adac353
Evgenii Shuklin <shuklineg@gmail.com>
2019-05-02 19:23:53 +0500

    Fix sign up redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,4 +11,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     resource.is_a?(Admin) ? admin_tests_path : root_path
   end
+
+  def after_inactive_sign_up_path_for(_resource)
+    new_user_session_path
+  end
 end
