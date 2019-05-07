7ca7052263fdce38a9324de4c60077612667da46
Evgenii Shuklin <shuklineg@gmail.com>
2019-05-07 11:10:30 +0500

    Questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,6 @@
 module QuestionsHelper
   def question_header(question)
-    action = question.new_record? ? 'Create' : 'Edit'
-    "#{action} #{question.test&.title} Question"
+    action = question.new_record? ? t('.create') : t('.edit')
+    t('.question_title', action: action, test: question.test&.title)
   end
 end
